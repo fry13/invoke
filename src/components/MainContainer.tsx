00fe2b9442ee8243c562357ec3ebf77785e36e27
Alexey Feklyunin <ninetailed1995@gmail.com>
2022-12-09 19:57:10 +0400

    Сorrect Mistake

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function MainContainer({ Children }: any{ Children }: any) {
+export default function MainContainer({ Children }: any) {
   return (
     <div className="w-1/4 h-1/3 mx-auto text-center rounded p-2 pb-8 shadow-lg border-2 border-slate-500/25">
       {Children}
