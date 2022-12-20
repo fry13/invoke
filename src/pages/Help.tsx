74a6677a1e0a259c6fd98c4e6cfb16922195fb3d
Alexey Feklyunin <ninetailed1995@gmail.com>
2022-12-20 06:01:22 +0400

    Add First Iteration Of FAQ

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Navigation from "../components/Navigation";
+import Orb from "../components/Orb";
+import Spell from "../components/Spell";
+import { spells } from "../spells";
 
 export default function HelpPage() {
+  let spellsArr = spells.map(function (spell) {
+    return (
+      <li className="w-1/3" key={spell.id}>
+        <Spell spell={spell} imgClasses={"w-10"} textClasses="hidden" />
+        <div className="flex justify-center mt-1">
+          <Orb classes="h-4" button={spell.buttons[0][0]} />
+          <Orb classes="h-4" button={spell.buttons[0][1]} />
+          <Orb classes="h-4" button={spell.buttons[0][2]} />
+        </div>
+      </li>
+    );
+  });
   return (
-    <div>HelpPage</div>
-  )
+    <>
+      <Navigation exit={true} />
+      <ul className="flex flex-wrap justify-around text-center h-full content-around">
+        {spellsArr}
+      </ul>
+    </>
+  );
 }
