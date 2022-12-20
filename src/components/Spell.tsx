2eb4c3ac636b209c85e8bfb9d7dcae2fcf60537f
Alexey Feklyunin <ninetailed1995@gmail.com>
2022-12-20 11:15:28 +0400

    Add Some Optimization

--- src/components/Spell.tsx
+++ src/components/Spell.tsx
@@ -12,7 +12,7 @@ export default function Icon({ spell, imgClasses, textClasses }: SpellProps) {
     <div>
       <img
         key={spell.id}
-        className={`mx-auto rounded border w-20 border-slate-800 transition-all ${imgClasses}`}
+        className={`mx-auto rounded border w-20 h-20 bg-slate-800 border-slate-800 transition-all ${imgClasses}`}
         src={spell.image}
         alt={spell.name}
       />
